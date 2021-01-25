413860964b935cb115fad5608acba8f6abb16738
Jumaydev <osipovm33rus@gmail.com>
2021-01-25 14:49:09 +0300

    подготовка к демо

--- server-config.php
+++ server-config.php
@@ -3,6 +3,6 @@
 define('HOST', 'http://calc:81/');
 
 define('SMTP', 'ssl://smtp.yandex.ru');
-define('USER', 'osipovm33rus@yandex.ru');
-define('PASSWORD', '345125Daxanavar');
+define('USER', '');
+define('PASSWORD', '');
 define('PORT', 465);
